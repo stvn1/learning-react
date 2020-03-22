8f43b35fc32f21854d86fa6cbd089726e8c65e33
stvn1 <esteban17100@gmail.com>
2020-03-21 20:56:18 -0400

    Working with attributes and expressions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
-    return <h1>Hello world!</h1>;
+    return (
+      <div>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </div>
+
+      /*
+      <React.Fragment>
+        <h1>Hello world!</h1>
+        <button>Increment</button>
+      </React.Fragment>
+      */
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    const x = <h1>Zero</h1>;
+    //return count === 0 ? <h1>Zero</h1> : this.state.count;
+    //return count === 0 ? x : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
